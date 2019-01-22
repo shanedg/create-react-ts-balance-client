77b81cec2252aba5d83fe938792bb7b53c97983b
Shane Garrity <shanedgarrity@gmail.com>
2019-01-21 20:06:13 -0500

    extract CustomForm into own class file

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@
 // tslint:disable-next-line:import-name
 import React, { Component } from 'react';
 
-import CustomInput from './CustomInput';
+import CustomForm from './CustomForm';
 
 import './App.scss';
 
@@ -17,57 +17,6 @@ class App extends Component {
         <CustomForm></CustomForm>
       </div>
     );
-  }
-}
-
-interface ICustomFormState {
-  input: any;
-}
-
-class CustomForm extends Component<{}, ICustomFormState> {
-  constructor(props: any) {
-    super(props);
-
-    this.state = {
-      input: '',
-    };
-
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-
-  public render() {
-    return (
-      <form onSubmit={this.handleSubmit}>
-        <CustomInput
-          label="asdf"
-          onChange={this.handleChange}
-          input={this.state.input}
-        ></CustomInput>
-        <input
-          type="submit"
-          value="Submit"
-        />
-      </form>
-    );
-  }
-
-  /**
-   * Update state based on change to input.
-   * @param {any} event Input change event.
-   */
-  private handleChange(event: any) {
-    this.setState({
-      input: event.target.value,
-    });
-  }
-
-  /**
-   * Handle form submission.
-   * @param {any} event Form submit event.
-   */
-  private handleSubmit(event: any) {
-    event.preventDefault();
   }
 }
 
---
+++ src/CustomForm.tsx
@@ -0,0 +1,57 @@
+// tslint:disable-next-line:import-name
+import React, { Component } from 'react';
+
+import CustomInput from './CustomInput';
+
+interface ICustomFormState {
+  input: any;
+}
+
+class CustomForm extends Component<{}, ICustomFormState> {
+  constructor(props: any) {
+    super(props);
+
+    this.state = {
+      input: '',
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  public render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <CustomInput
+          label="asdf"
+          onChange={this.handleChange}
+          input={this.state.input}
+        ></CustomInput>
+        <input
+          type="submit"
+          value="Submit"
+        />
+      </form>
+    );
+  }
+
+  /**
+   * Update state based on change to input.
+   * @param {any} event Input change event.
+   */
+  private handleChange(event: any) {
+    this.setState({
+      input: event.target.value,
+    });
+  }
+
+  /**
+   * Handle form submission.
+   * @param {any} event Form submit event.
+   */
+  private handleSubmit(event: any) {
+    event.preventDefault();
+  }
+}
+
+export default CustomForm;
